351b0b1c5e19b4b2d1c80d113dc304fcc1bc17f3
dewei zhang <377805163@qq.com>
2021-05-11 23:41:55 +0800

    5-10

--- main.js
+++ main.js
@@ -1,40 +1,36 @@
 
-const {app, BrowserWindow,Notification} = require('electron')
+const { app, BrowserWindow  } = require('electron')
 const path = require('path')
 
-
-
 function createWindow () {
-  
-  const mainWindow = new BrowserWindow({
+  const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
-  mainWindow.loadFile('index.html')
-}
 
-function showNotification (){
-  const notification = {
-    title: 'Basic Notification',
-    body: 'Notification from the Main process'
-  }
-  new Notification(notification).show()
+  win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
   createWindow()
-  
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-}).then(showNotification)
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
 })
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.setUserTasks([])
 
 
 
